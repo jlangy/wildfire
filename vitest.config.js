f331b69eb05a54a2f5a7e946b1ecc8da2347c0e2
Jonathan Langlois <jonathan-langlois@live.ca>
2023-08-13 14:23:02 -0700

    vitest config

---
+++ vitest.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    deps: {
+      moduleDirectories: ["node_modules", "./node_modules"],
+    },
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      components: "./src/components",
+      utils: "./src/utils",
+    },
+  },
+  dedupe: ["react", "react-dom"]
+});
